1ff412e4f84211e4161a39da30129e7a5d1a8cf7
Michael Kranz <mbkranz@gmail.com>
2024-06-09 21:53:18 -0500

    port constant vars to config.py

--- package.py
+++ package.py
@@ -6,17 +6,7 @@ from datetime import datetime, timezone
 from utils import drop_excluded_fields, write_data_package
 from frictionless import Schema, Resource, Package
 import cleaning
-
-VERSION = "2.0"
-DATASETS = [
-    {"name": "survey1", "filename": "omnibus_survey1_2020-02_072821"},
-    {"name": "survey2", "filename": "omnibus_survey2_2020-04_072821"},
-    {"name": "survey3", "filename": "omnibus_survey3_2020-06_072821"},
-    {"name": "survey4", "filename": "omnibus_survey4_2020-10_072821"},
-    {"name": "survey5", "filename": "omnibus_survey5_2021-02_072821"},
-    {"name": "survey7", "filename": "omnibus_survey7_2022-04"},
-    {"name": "survey8", "filename": "omnibus_survey8_2022-12"},
-]
+from config import DATASETS,VERSION
 
 resources = []
 for dataset in DATASETS:
